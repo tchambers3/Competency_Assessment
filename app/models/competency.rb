4f58723811bc995c2f08ff2a8d5919cb6b5aba5c
vagrant <vagrant@vagrant.vm>
2017-03-08 07:14:42 +0000

    Added validations and scopes to the competency model

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -1,2 +1,14 @@
 class Competency < ActiveRecord::Base
+    # Relationships
+    # has_many :indicators
+
+    # Validations
+    validates_presence_of :name
+
+
+    # Scopes
+    scope :alphabetical, -> { order("name") }
+    scope :active, -> { where("active = ?", true) }
+    scope :inactive, -> { where("active = ?", false) }
+
 end
