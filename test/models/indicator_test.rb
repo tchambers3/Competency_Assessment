7ee2a2f869d0a5246f9bb09870ac3814c33ddeb7
Richard Huang <rahuang@andrew.cmu.edu>
2017-03-10 21:05:07 +0000

    Added more scope tests for indicator model

--- test/models/indicator_test.rb
+++ test/models/indicator_test.rb
@@ -5,6 +5,7 @@ class IndicatorTest < ActiveSupport::TestCase
   should belong_to(:level)
   should belong_to(:competency)
 
+
   # Basic Validations
   should validate_presence_of(:description)
   should validate_presence_of(:level_id)
@@ -14,10 +15,81 @@ class IndicatorTest < ActiveSupport::TestCase
 
   should allow_value("Indicator 1").for(:description)
   should_not allow_value("").for(:description)
+
   should allow_value(1).for(:level_id)
+  should_not allow_value(5.3).for(:level_id)
   should_not allow_value("").for(:level_id)
   should_not allow_value("id").for(:level_id)
+
   should allow_value(1).for(:competency_id)
   should_not allow_value("").for(:competency_id)
-  should_not allow_value("id").for(:level_id)
+  should_not allow_value("id").for(:competency_id)
+  should_not allow_value(7.5).for(:competency_id)
+
+
+  # Scope and method tests
+  context "With a proper context, " do
+    setup do
+      create_competencies
+      create_levels
+      create_indicators
+    end
+
+    teardown do
+      remove_competencies
+      remove_levels
+      remove_indicators
+    end
+
+    # Test that objects are created properly
+    should "show that all factory objects are properly created" do
+      assert_equal false, @indicator1.nil?
+      assert_equal "Able to identify common nonverbal cues.", @indicator1.description
+      assert_equal "Able to present written communication in an easy–to-read format.", @indicator2.description
+      assert_equal "Communication", @indicator3.competency.name
+      assert_equal "Companion", @indicator4.level.name
+      assert_equal false, @indicator5.active
+    end
+
+    # test alphabetical scope
+    should "have indicators listed alphabetically" do
+      assert_equal ["Able to identify apparent causes of a problem.", 
+        "Able to identify common nonverbal cues.",
+        "Able to outline a plan to gather data that will aid in the completion of a familiar task.", 
+        "Able to present written communication in an easy–to-read format.", 
+        "Engages in difficult conversations with others while maintaining respect."], 
+        Indicator.alphabetical.map { |e| e.description }
+    end
+
+    # test by_level scope
+    should "have indicators listed in order by level ranking" do
+      assert_equal ["Engages in difficult conversations with others while maintaining respect.",
+        "Able to identify apparent causes of a problem.",
+        "Able to present written communication in an easy–to-read format.",
+        "Able to identify common nonverbal cues.",
+        "Able to outline a plan to gather data that will aid in the completion of a familiar task."], 
+        Indicator.by_level.alphabetical.map { |e| e.description }
+    end
+
+    # test by_competency scope
+    should "have indicators listed in order by competency name" do
+      assert_equal ["Able to identify common nonverbal cues.",
+        "Able to present written communication in an easy–to-read format.",
+        "Engages in difficult conversations with others while maintaining respect.",
+        "Able to outline a plan to gather data that will aid in the completion of a familiar task.",
+        "Able to identify apparent causes of a problem."], 
+        Indicator.by_competency.alphabetical.map { |e| e.description }
+    end
+
+    # test for_level scope
+    should "have indicators listed for a certain level" do
+      assert_equal ["Able to identify apparent causes of a problem.",
+        "Able to present written communication in an easy–to-read format."],
+        Indicator.for_level("Contributer").alphabetical.map { |e| e.description }
+
+      assert_equal ["Engages in difficult conversations with others while maintaining respect."],
+        Indicator.for_level("Champion").alphabetical.map { |e| e.description }        
+    end
+
+  end
 end
