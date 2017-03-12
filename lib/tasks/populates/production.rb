2be0dd890c7fbb20bd5dbdfa6ae886cfc0be94c0
Richard Huang <rahuang@andrew.cmu.edu>
2017-03-12 09:02:47 +0000

    Created git populate file and setup for competency, level, and indicator models.

---
+++ lib/tasks/populates/production.rb
@@ -0,0 +1,18 @@
+# For production environment only populate these models:
+# Level, Paradigm, User
+
+# Step 1: Create the Levels
+levels = 
+  [
+    ["Champion", "The highest level of indicators.", 1, true],
+    ["Contributer", "The middle level of indicators.", 2, true],
+    ["Contributer", "The lowest level of indicators.", 3, true]
+  ]
+levels.each do |level|
+  l = Level.new
+  l.name = level[0]
+  l.description = level[1]
+  l.ranking = level[2]
+  l.active = level[3]
+  l.save!
+end
