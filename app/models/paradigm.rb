3761fb78393ab519e17f3c386d5a3d22576e5baa
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-15 16:16:25 -0400

    Scopes and validations added to paradigm model

--- app/models/paradigm.rb
+++ app/models/paradigm.rb
@@ -1,2 +1,12 @@
 class Paradigm < ActiveRecord::Base
+
+  #Validations
+  validates_presence_of :name
+  validates_presence_of :description
+
+  #Scopes
+  scope :alphabetical, -> { order("name") }
+  scope :active, -> { where("active = ?", true) }
+  scope :inactive, -> { where("active = ?", false) }
+
 end
