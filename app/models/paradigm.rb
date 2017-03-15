d9ca1763322d19ede2c5af83cbb100969288239f
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-15 18:18:46 -0400

    Ranking scope and validation added for paradigm model

--- app/models/paradigm.rb
+++ app/models/paradigm.rb
@@ -3,10 +3,13 @@ class Paradigm < ActiveRecord::Base
   #Validations
   validates_presence_of :name
   validates_presence_of :description
+  validates_presence_of :ranking
 
   #Scopes
   scope :alphabetical, -> { order("name") }
-  scope :active, -> { where("active = ?", true) }
-  scope :inactive, -> { where("active = ?", false) }
+  scope :desc_rank, -> { order('ranking DESC') }
+  scope :asc_rank, -> { order('ranking ASC') }
+  scope :active, -> { where('active = ?', true) }
+  scope :inactive, -> { where('active = ?', false) }
 
 end
