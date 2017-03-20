4f3993d7893e0ffb8985211e44b914dcf52ca963
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-20 15:30:39 -0400

    Add relationship and fix scope ordering

--- app/models/paradigm.rb
+++ app/models/paradigm.rb
@@ -1,15 +1,18 @@
 class Paradigm < ActiveRecord::Base
 
+  #Relationships
+  belongs_to :resource
+
   #Validations
-  validates_presence_of :name
-  validates_presence_of :description
-  validates_presence_of :ranking
+  validates_presence_of :name, :description, :ranking
 
   #Scopes
   scope :alphabetical, -> { order("name") }
-  scope :desc_rank, -> { order('ranking DESC') }
   scope :asc_rank, -> { order('ranking ASC') }
+  scope :desc_rank, -> { order('ranking DESC') }
   scope :active, -> { where('active = ?', true) }
   scope :inactive, -> { where('active = ?', false) }
 
+  #TODO: Implement methods for ranking alteration based on user input
+  
 end
