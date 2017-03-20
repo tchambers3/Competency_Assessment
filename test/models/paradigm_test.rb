0aa35815dba4ccc2320796c8fc0309178503f1dd
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-20 16:15:44 -0400

    Added validation tests

--- test/models/paradigm_test.rb
+++ test/models/paradigm_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ParadigmTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #TODO: test relationship once its added
+
+  # Validation tests
+  should validate_presence_of(:name)
+  should validate_presence_of(:description)
+  should validate_presence_of(:ranking)
+  should validate_numericality_of(:ranking)
+  should validate_uniqueness_of(:ranking)
+
+  context "With a proper context," do
+    setup do
+      create_paradigms
+    end
+
+    teardown do
+      remove_paradigms
+    end
+
+    should "Create all test objects properly" do
+      assert_equal false, @build_understanding.nil?
+      assert_equal "Do Something", @do_something.name
+      assert_equal "Get Connected", @get_connected.name
+      assert_equal "Learn More", @learn_more.name
+    end
+    
+  end
+
 end
