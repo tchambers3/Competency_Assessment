b2b5c93a4d0b30b014f5afce7007dcc0cb74baa9
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-23 16:29:00 -0400

    Add resource tests

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class ResourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Test Relationships
+  should have_one(:paradigm)
+
+  # Test Validations
+  should validate_presence_of(:title, :link)
+  should validates_uniqueness_of(:title, :link)
+
+  # Test scopes
+  context "With a proper context" do
+    setup do
+      create_paradigms
+      create_resources
+    end
+
+    teardown do
+      remove_resources
+      remove_paradigms
+    end
+
+    should "show that all resources are created properly" do
+      assert_equal false, @comm_dumm.nil?
+      assert_equal "How to Succeed in Business", @success.title
+      assert_equal "How to Fail in Business", @failure.title
+      assert_equal "Public Speaking 101 Workshop", @pub_speak.title
+    end
+
+    should "have all resources listed alphabetically" do
+      assert_equal ["Communication Skills for Dummies", "How to Succeed in Business", "How to Fail in Business", "Public Speaking 101 Workshop"], Resource.alphabetical.map  { |e| e.name }
+    end
+
+
+  end
+
+
 end
