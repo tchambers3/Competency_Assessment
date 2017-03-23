64f869143537d890b392c8f4fc154f8e1e9493df
Richard Huang <rahuang@andrew.cmu.edu>
2017-03-23 06:14:04 +0000

    Added routes and built out framework for levels controller.

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,64 @@
+class CompetenciesController < ApplicationController
+  # Callback Methods
+  before_action :set_level, only: [:show, :edit, :update, :destroy]
+
+  # GET /levels
+  def index
+    @active_levels = Level.active.alphabetical
+    @inactive_levels = Level.inactive.alphabetical
+  end
+
+  # GET /levels/:id
+  def show
+  end
+
+  # GET /levels/new
+  def new
+    @level = Level.new
+  end
+
+  # POST /levels
+  def create
+    @level = Level.new(level_params)
+    if @level.save
+      flash[:notice] = "Successfully created #{@level.name}"
+      redirect_to level_path(@level)
+    else
+      render "new"
+    end
+  end
+
+  # GET /levels/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /levels/:id
+  def update
+    if @level.update(level_params)
+      flash[:notice] = "Successfully updated #{@level.name}"
+      redirect_to level_path(@level)
+    else
+      render "edit"
+    end
+  end
+
+  # DELETE /levels/:id
+  def destroy
+    @level.destroy
+    flash[:notice] = "Successfully deleted #{@level.name}"
+    redirect_to levels_path
+  end
+
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_level
+    @level = Level.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def level_params
+    params.require(:level).permit(:name, :description, :ranking, :active)
+  end
+end
