1122caea41f11d3e36be1ee3e399611e70059f55
Richard Huang <rahuang@andrew.cmu.edu>
2017-03-25 05:47:18 +0000

    Reformatted the imports controller and refactored. Will need more work.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -4,6 +4,8 @@ class ImportsController < ApplicationController
 
   def parse
     file = params[:file]
+    # Check if the the file is of the right type.
+    # Redirect to the original upload url early if it's not correct.
     begin
       spreadsheet = open_spreadsheet(file)
     rescue => exception
@@ -11,13 +13,25 @@ class ImportsController < ApplicationController
       return redirect_to root_url
     end
 
+    # Parses each fo the sheets
+    # Returns a list of ActiveRecords that need to be saved into the database
+    # For specific models like Level, it returns a list of all levels listed in 
+    # the Excel file as well as the the list of new objects that need to be saved.
+    # This is because the same exact levels will generally be in every Excel file, but
+    # we don't need to redundantly save it into the database each time. 
     competencies = parse_competencies(spreadsheet)
     levels, new_levels = parse_levels(spreadsheet)
-    byebug
-    if save_models(competencies, levels, new_levels)
+    
+    # Validate that the new models are all valid and save them to the database.
+    # If any of the new models are not valid, the aggregate_errors method will be
+    # called to aggregate all the errors that need to be fixed.
+    if validate_save_models(competencies, new_levels)
       flash[:notice] = "Successfully uploaded and imported the #{file.original_filename} spreadsheet."
+    else
+      aggregate_errors(competencies, levels)
     end
 
+    # Redirect to the original page regardless of success
     redirect_to root_url
   end
 
@@ -35,7 +49,7 @@ class ImportsController < ApplicationController
       competency.attributes = c.to_hash
       competencies << competency
     end
-    competencies
+    return competencies
   end
 
   def parse_levels(spreadsheet)
@@ -58,24 +72,32 @@ class ImportsController < ApplicationController
     return levels, new_levels
   end
 
-  def save_models(competencies, levels, new_levels)
+  # Takes in the list of all models that need to be saved into the database.
+  # Checks if the new objects are valid, before saving into the databse.
+  # Nothing is saved unless everything is valid.
+  def validate_save_models(competencies, new_levels)
     if competencies.map(&:valid?).all? && new_levels.map(&:valid?).all?
       competencies.each(&:save!)
       new_levels.each(&:save!)
-      true
-    else
-      flash[:error] = []
-      competencies.each_with_index do |competency, index|
-        competency.errors.full_messages.each do |message|
-          flash[:error] << "Competencies Sheet - Row #{index+2}: #{message}"
-        end
+      return true
+    end
+    return false
+  end
+
+  # This method is called if an error exists in the list of models.
+  # Goes through all the models in order to aggregate the errors into flash[:error]
+  # Didn't use the new models (ex. new_levels vs. levels) because of Row #'s for error message.
+  def aggregate_errors(competencies, levels)
+    flash[:error] = []
+    competencies.each_with_index do |competency, index|
+      competency.errors.full_messages.each do |message|
+        flash[:error] << "Competencies Sheet - Row #{index+2}: #{message}"
       end
-      levels.each_with_index do |level, index|
-        level.errors.full_messages.each do |message|
-          flash[:error] << "Levels Sheet - Row #{index+2}: #{message}"
-        end
+    end
+    levels.each_with_index do |level, index|
+      level.errors.full_messages.each do |message|
+        flash[:error] << "Levels Sheet - Row #{index+2}: #{message}"
       end
-      false
     end
   end
 
