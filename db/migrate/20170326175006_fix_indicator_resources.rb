5d7a98bf560e6d44d89f072fff410c0abcf5e11b
Piyush Puri <ppuri@andrew.cmu.edu>
2017-03-26 21:02:31 -0400

    Fix extra line in migrate file

--- db/migrate/20170326175006_fix_indicator_resources.rb
+++ db/migrate/20170326175006_fix_indicator_resources.rb
@@ -1,6 +1,5 @@
 class FixIndicatorResources < ActiveRecord::Migration
   def change
     rename_column :indicator_resources, :indictaor_id, :indicator_id
-    change_column :resources, :active, :boolean, :default => true
   end
 end
