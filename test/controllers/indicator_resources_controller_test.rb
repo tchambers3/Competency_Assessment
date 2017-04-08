7e490786827ee595740c0842ab87a04ee3c4b60c
Piyush Puri <ppuri@andrew.cmu.edu>
2017-04-07 21:56:27 -0400

    Add new file for IR controller and start tests

---
+++ test/controllers/indicator_resources_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class IndicatorResourcesControllerTest < ActionController::TestCase
+  # Test all IndicatorResourcesController methods
+
+  setup do
+    create_competencies
+    create_levels
+    create_indicators
+    create_resources
+    create_indicator_resources
+  end
+
+  # Test that create action creates a valid object
+  test "should create indicator_resource" do
+
+  end
+
+  # Test that the create action does not create an invalid object 
+
+
+end
