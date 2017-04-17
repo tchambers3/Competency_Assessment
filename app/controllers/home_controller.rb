9de768c02d57198917b1d261c42c66032f987d07
tchambers3 <tjchambers8@gmail.com>
2017-04-17 00:19:30 -0400

    Add before_action check to home controller with exceptions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,20 @@
 class HomeController < ApplicationController
+  before_action :check_login, :except => [:home]
 
   # GET / (a.k.a root_path)
   def home
   end
 
-<<<<<<< HEAD
-  def dashboard
-    @active_competencies = Competency.active.alphabetical
-    @inactive_competencies = Competency.inactive.alphabetical
-    @levels = Level.active.by_ranking
-    @paradigms = Paradigm.active.by_ranking
-=======
   # GET /about
   def about
   end
 
   # GET /dashboard
   def dashboard
->>>>>>> 21c333eb2781703dfd11d8400c4c30f9c481fd66
+    @active_competencies = Competency.active.alphabetical
+    @inactive_competencies = Competency.inactive.alphabetical
+    @levels = Level.active.by_ranking
+    @paradigms = Paradigm.active.by_ranking
   end
 
 end
