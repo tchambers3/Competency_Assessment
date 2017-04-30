2392d6ad9e20d469a0c73757ca18ef0f7052b4fe
Piyush Puri <ppuri@andrew.cmu.edu>
2017-04-30 17:20:17 -0400

    Back button LOL

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,7 @@ class QuestionsController < ApplicationController
     # Get questions related to the competency
     @active_questions = Question.active.for_competency(params[:id])
     @inactive_questions = Question.inactive.for_competency(params[:id])
+    @competency_id = params[:id]
   end
 
   # GET /questions/:id
@@ -18,6 +19,7 @@ class QuestionsController < ApplicationController
   # GET /questions/new
   def new
     @question = Question.new
+    @competency_id = params[:id]
   end
 
   # POST /questions
