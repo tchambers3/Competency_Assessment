c3829293764c74f432adaa86ca38ea94e2923c74
Piyush Puri <ppuri@andrew.cmu.edu>
2017-04-30 23:07:13 -0400

    Admin layout added to many to many controllers, bug fix for back buttons

--- app/models/indicator_question.rb
+++ app/models/indicator_question.rb
@@ -12,14 +12,14 @@ class IndicatorQuestion < ActiveRecord::Base
   # Methods
   def self.parse(spreadsheet, indicators, questions)
     indicator_questions_sheet = spreadsheet.sheet("Indicator_Questions")
-    indicator_questions_hash = 
+    indicator_questions_hash =
       indicator_questions_sheet.parse(indicator_id: "Indicator_ID", question_id: "Question_ID")
 
     indicator_questions = []
     indicator_questions_hash.each_with_index do |iq, index|
       indicator_question = IndicatorQuestion.new
 
-      # Used to set the foreign keys. 
+      # Used to set the foreign keys.
       # indicator_id and question_id should be the id of the indicator and question
       # relative to the row number of indicator and question sheets
       # the offset is because of 0 indexing plus the header in the excel file
@@ -29,7 +29,7 @@ class IndicatorQuestion < ActiveRecord::Base
 
       qid = iq[:question_id]
       iq[:question_id] = (qid.nil? || questions[qid - 2].nil?) ? nil : questions[qid - 2].id
-      
+
       indicator_question.attributes = iq.to_hash
       indicator_questions << indicator_question
     end
