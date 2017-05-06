be7b721a6f324b4ff752bf51adf8f678308e1cff
tchambers3 <tjchambers8@gmail.com>
2017-05-06 15:17:18 -0400

    Fix spelling errors

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -22,7 +22,7 @@ class AssessmentsController < ApplicationController
   def send_email
     @email = params[:email]
     @link = request.referer
-    AssesmentMailer.send_assesment(@email,@link).deliver_now
+    AssessmentMailer.send_assessment(@email,@link).deliver_now
     redirect_to @link
   end
 
---
+++ test/mailers/assessment_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AssessmentMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
